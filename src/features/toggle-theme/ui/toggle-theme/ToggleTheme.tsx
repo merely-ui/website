b7ce0f912281d55dc4a4caedd5a70a3eb62238fb
Ivan Blinov <blinovivan2016@gmail.com>
2024-09-08 10:13:51 +0300

    refactor: update selects in header

--- src/features/toggle-theme/ui/toggle-theme/ToggleTheme.tsx
+++ src/features/toggle-theme/ui/toggle-theme/ToggleTheme.tsx
@@ -7,7 +7,7 @@ function ToggleTheme() {
 	const { theme } = useColorTheme()
 
 	return (
-		<Select>
+		<Select minW={110}>
 			<SelectItem selected={theme === 'dark'}>Dark</SelectItem>
 			<SelectItem selected={theme === 'light'}>Light</SelectItem>
 		</Select>
--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,7 +1,7 @@
 import { SearchBar } from '@/src/features/search-docs'
 import { ToggleTheme } from '@/src/features/toggle-theme'
 import Container from '@/src/shared/ui/Container'
-import { Button, Flex, Select } from '@merely-ui/react'
+import { Button, Flex, Select, SelectItem } from '@merely-ui/react'
 import Link from 'next/link'
 import Logo from './Logo'
 
@@ -20,7 +20,10 @@ export default function Header() {
 				<SearchBar />
 				<Flex alignItems='center' gap={25}>
 					<Flex gap={10}>
-						<Select maxW={50}></Select>
+						<Select minW={85}>
+							<SelectItem selected>En</SelectItem>
+							<SelectItem>Ру</SelectItem>
+						</Select>
 						<ToggleTheme />
 					</Flex>
 					<a href='https://boosty.to/blinovvv/donate'>
