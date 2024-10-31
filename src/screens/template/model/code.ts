e6f5c7d9ef00c4cd079cc645a31a14e0184e44e5
Ivan Blinov <blinovivan2016@gmail.com>
2024-10-31 11:19:05 +0300

    feat: add box docs

---
+++ src/screens/template/model/code.ts
@@ -0,0 +1,7 @@
+export const ImportCode = `import { Template } from "@merely-ui/react"
+import { Box } from "@merely-ui/react"`
+
+export const UsageCode = `<Flex>
+	<Template w={30} height={30} bgColor={colors.cyan.$200}  />
+	<Box w={30} height={30} bgColor={colors.cyan.$200}  />
+</Flex>`
--- src/screens/template/ui/TemplatePage.tsx
+++ src/screens/template/ui/TemplatePage.tsx
@@ -1,16 +1,51 @@
+import ShikiCodeBlock from '@/src/shared/lib/shiki/ShikiCodeBlock'
 import { DocsPage, SidebarTitles } from '@/src/widgets/docs-page'
+import {
+	Box,
+	Card,
+	colors,
+	Flex,
+	Template,
+	Text,
+	Title,
+} from '@merely-ui/react'
+import { ImportCode, UsageCode } from '../model/code'
 
 const sidebarTitles: SidebarTitles = [
 	{
-		text: '',
-		idAttr: '',
+		text: 'Import',
+		idAttr: 'import',
+	},
+	{
+		text: 'Usage',
+		idAttr: 'usage',
 	},
 ]
 
 export default function TemplatePage() {
 	return (
 		<DocsPage sidebarTitles={sidebarTitles}>
-			
+			<Title fontSize={30} fontWeight={700} mb={20}>
+				Template (Box)
+			</Title>
+			<Text mb={16}>
+				Template component is used to create your own components. Renders blank
+				`div` by default.
+			</Text>
+			<Title scrollMarginTop={125} id='import' size='2xl' mb={12} mt={32}>
+				Import
+			</Title>
+			<ShikiCodeBlock code={ImportCode} />
+			<Title id='usage' size='2xl' mb={12} mt={32}>
+				Usage
+			</Title>
+			<Card mb={8}>
+				<Flex>
+					<Template w={30} height={30} bgColor={colors.cyan.$200} />
+					<Box w={30} height={30} bgColor={colors.purple.$200} />
+				</Flex>
+			</Card>
+			<ShikiCodeBlock code={UsageCode} />
 		</DocsPage>
 	)
 }
